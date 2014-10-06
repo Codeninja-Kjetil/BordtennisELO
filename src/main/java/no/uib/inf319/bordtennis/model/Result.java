0d9829a4a48c424b2cf2d3e9e6fd5d9bcf9c3163
Codeninja-Kjetil <harry200200@hotmail.com>
2014-10-06 20:50:19 +0200

    Added comments to Result

--- src/main/java/no/uib/inf319/bordtennis/model/Result.java
+++ src/main/java/no/uib/inf319/bordtennis/model/Result.java
@@ -62,42 +62,82 @@ public class Result implements Serializable {
     public Result() {
     }
 
+    /**
+     * Gets {@link #resultid}.
+     * @return resultid
+     */
     public final Integer getResultid() {
         return this.resultid;
     }
 
+    /**
+     * Sets {@link #resultid}.
+     * @param resultid resultid
+     */
     public final void setResultid(final Integer resultid) {
         this.resultid = resultid;
     }
 
+    /**
+     * Gets {@link #elo}.
+     * @return elo
+     */
     public final Integer getElo() {
         return this.elo;
     }
 
+    /**
+     * Sets {@link #elo}.
+     * @param elo elo
+     */
     public final void setElo(final Integer elo) {
         this.elo = elo;
     }
 
+    /**
+     * Gets {@link #playernumber}.
+     * @return playernumber
+     */
     public final Integer getPlayernumber() {
         return this.playernumber;
     }
 
+    /**
+     * Sets {@link #playernumber}.
+     * @param playernumber playernumber
+     */
     public final void setPlayernumber(final Integer playernumber) {
         this.playernumber = playernumber;
     }
 
+    /**
+     * Gets {@link #match}.
+     * @return match
+     */
     public final Match getMatch() {
         return this.match;
     }
 
+    /**
+     * Sets {@link #match}.
+     * @param match match
+     */
     public final void setMatch(final Match match) {
         this.match = match;
     }
 
+    /**
+     * Gets {@link #player}.
+     * @return player
+     */
     public final Player getPlayer() {
         return this.player;
     }
 
+    /**
+     * Sets {@link #player}.
+     * @param player player
+     */
     public final void setPlayer(final Player player) {
         this.player = player;
     }
